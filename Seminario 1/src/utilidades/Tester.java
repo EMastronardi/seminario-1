0f24c84e3fa8eb8eea13c6460598f7ab3728a539
emolinero <emolinero@gmail.com>
2014-02-20 03:03:50 +0000

    Clase con método main para hacer la carga de datos iniciales

--- Seminario 1/src/utilidades/Tester.java
+++ Seminario 1/src/utilidades/Tester.java
@@ -1,5 +1,12 @@
 package utilidades;
 
+import org.hibernate.Session;
+
+import persistencia.HibernateUtil;
+
 public class Tester {
-	
+	public static void main(String[] args){
+		System.out.println("Carga de los datos de prueba");
+		InitializeSystems.cargador();
+	}
 }
